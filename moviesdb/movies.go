2d30610d0219221323c8045745e3865989ad8fb3
kokopiotrowski <konrad.piotrowski00@gmail.com>
2021-06-04 00:37:16 +0200

    people

---
+++ moviesdb/movies.go
@@ -0,0 +1,11 @@
+package moviesdb
+
+import swagger "studies/SEP6-Backend/swagger/models"
+
+const (
+	movieCastURL = "/movie/{movie_id}/credits"
+)
+
+func GetCastForMovie(movie *swagger.Movie) {
+
+}
