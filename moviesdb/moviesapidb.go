09cadd46b0eb66754fe48297dc14a2fd0cdd3f0a
kokopiotrowski <konrad.piotrowski00@gmail.com>
2021-06-03 19:23:28 +0200

    yaaaml

--- moviesdb/moviesapidb.go
+++ moviesdb/moviesapidb.go
@@ -2,12 +2,9 @@ package moviesdb
 
 import (
 	swagger "studies/SEP6-Backend/swagger/models"
-	"studies/SEP6-Backend/util"
 )
 
 func MoviesGetPopular() ([]swagger.Movie, error) {
-
-	util.SendRequest()
 
 	return nil, nil
 }
